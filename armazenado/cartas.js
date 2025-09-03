3fef45232c145c7e3205fe7090bd597901324c6e
Luann Eliseu <110840769+FleyLT@users.noreply.github.com>
2025-09-03 13:41:45 -0300

    Create cartas.js

---
+++ armazenado/cartas.js
@@ -0,0 +1,7 @@
+var cartas = [
+{"numero": "", "cor": "verde", "imagem": ""},
+{"numero": "", "cor": "azul", "imagem": ""},
+{"numero": "", "cor": "vermelho", "imagem": ""},
+{"numero": "", "cor": "amarelo", "imagem": ""},
+
+]
